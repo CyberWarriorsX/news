3ac5960dc60a5193a3cccb96e4d29bfa2bd2cf45
Cyber Srimal <145316619+CyberWarriorsX@users.noreply.github.com>
2023-12-09 10:53:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -68,18 +68,18 @@ ${data.time}
 
 🗞️ *News From hirunews.lk*
 
-🔗 *Create By Nipuna Rangana*
+🔗 *Create By Dark Yasiya X Team*
 
-📍 *SL News*
+📍 *SL News Center*
 
-📰 *Group Link* :- https://chat.whatsapp.com/D0ywz4bax49AVnBLOZL7sI
+📰 *Group Link* :- https://chat.whatsapp.com/ECcDo6SuJbn0xb9gNLzrYP
 
 ●━━━━━━━━━━━━━━━━━━━━━●`
      let newss = await news1.findOne({ id: '123' }) 
   
      if (!newss) { 
          await new news1({ id: '123', newsid: data.id, events:'true' }).save() 
-           await session.sendMessage("120363175053901301@g.us",{image:{url: data.image}, caption:mg},{ ephemeralExpiration: WA_DEFAULT_EPHEMERAL })
+           await session.sendMessage("120363188209325112@g.us",{image:{url: data.image}, caption:mg},{ ephemeralExpiration: WA_DEFAULT_EPHEMERAL })
      } else { 
          if(newss.newsid == data.id )  
           { 
@@ -87,7 +87,7 @@ ${data.time}
           } 
           else{ 
              await news1.updateOne({ id: '123' }, { newsid : data.id, events:'true'}) 
-             await session.sendMessage("120363175053901301@g.us",{image:{url: data.image}, caption:mg},{ ephemeralExpiration: WA_DEFAULT_EPHEMERAL })
+             await session.sendMessage("120363188209325112@g.us",{image:{url: data.image}, caption:mg},{ ephemeralExpiration: WA_DEFAULT_EPHEMERAL })
           } 
   
      } 
